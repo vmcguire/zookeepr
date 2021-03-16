3c64857dcda3c055d250a2257c03df84ff4b7b5c
vmcguire <victor.mcguire@gmail.com>
2021-03-16 11:20:52 -0700

    updating some code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { animals } = require("./data/animals");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { animals } = require("./data/animals");
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
